5ff07a8218c77d57a7507b5d82a21cfc8b9e2887
Aleksey <lexa.beletsky.b@gmail.com>
2022-07-04 05:45:42 +0700

    [+] ClubHeader: Add bg

--- src/components/mobile/ClubHeader.vue
+++ src/components/mobile/ClubHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-between p-3 fixed w-full">
+  <div class="flex justify-between p-3 fixed w-full bg-slate-50">
     <div>Club</div>
     <div v-if="isMenuOpen" @click="closeMenu()">Назад</div>
     <div v-else @click="openMenu()">Меню</div>
