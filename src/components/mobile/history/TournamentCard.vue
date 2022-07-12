da4b3ec2ffd6a7f85e5431bf8d803151b84f4f55
Aleksey <lexa.beletsky.b@gmail.com>
2022-07-12 17:46:40 +0700

    [+] History: TournamentCard

--- src/components/mobile/history/TournamentCard.vue
+++ src/components/mobile/history/TournamentCard.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>
-    {{ item }}
+  <div class="bg-white p-2 rounded shadow-lg shadow-gray-100/50">
+    <div class="flex">
+      <CupSvg :size="32" :color="'#0F172A'" />
+      <div class="ml-2">Турнир от {{ item.created_at }}</div>
+    </div>
+    <div class="text-left mt-2 text-slate-500">
+      <span>Участников: {{ item.number_participants }} </span>
+    </div>
   </div>
 </template>
 
 <script>
+import CupSvg from "../../svg/CupSvg.vue";
+
 export default {
+  components: {
+    CupSvg,
+  },
   props: {
     item: Object,
   },
