1589aa967d3aa6a49baadf4870d75bd32f6cbdee
Aleksey <lexa.beletsky.b@gmail.com>
2022-08-21 14:48:11 +0700

    [+] History: Add TournamentCard

--- src/components/desktop/history/TournamentCard.vue
+++ src/components/desktop/history/TournamentCard.vue
@@ -1,9 +1,42 @@
 <template>
-  <div>TournamentCard</div>
+  <div
+    class="
+      flex
+      bg-white
+      py-2
+      px-4
+      rounded
+      shadow-lg shadow-gray-100/50
+      justify-between
+      items-center
+    "
+  >
+    <div class="flex items-center">
+      <CupSvg :size="32" :color="'#0F172A'" />
+      <div class="ml-2">Турнир от {{ item.created_at }}</div>
+      <span class="ml-4 text-slate-500"
+        >Участников: {{ item.number_participants }}
+      </span>
+    </div>
+    <div class="text-left">
+      <ArrowRight1Svg />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import CupSvg from "../../svg/CupSvg.vue";
+import ArrowRight1Svg from "../../svg/ArrowRight1Svg.vue";
+
+export default {
+  components: {
+    CupSvg,
+    ArrowRight1Svg,
+  },
+  props: {
+    item: Object,
+  },
+};
 </script>
 
 <style>
